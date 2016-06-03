abd2cff862b4379af2febcfd92fcf6315b979c70
Matthew Anderson <andersm2@union.edu>
2016-06-03 17:39:01 -0400

    CheckUSP compiles.

--- CheckUSP.c
+++ CheckUSP.c
@@ -6,7 +6,7 @@
 #include "permutation.h"
 
 typedef struct puzzle {
-  permutation pi;
+  permutation * pi;
   int ** puzzle;
   int row;
   int column;
@@ -57,53 +57,52 @@ puzzle * create_puzzle(int rows, int cols){
   usp -> puzzle[5][1] = 2;
   usp -> puzzle[5][1] = 3;
   
-
-  //usp -> pi = ID_permutaion(6);
+  usp -> pi = ID_permutation(6);
 
   return usp;
 }
 
 
-/* int CheckUSP(puzzle p){ */
-/*   permutation pi_1, pi_2, pi_3; */
-/*   int u, i, result; */
-/*   result = -1; */
+int CheckUSP(puzzle * p){
+  permutation * pi_1, * pi_2, * pi_3;
+  int u, i, result;
+  result = -1;
 
-/*   for (pi_1 = ID_permutation(p->row); pi_1!=last_permutation(ID_permutation(p->row)); pi_1 = next_permutation(pi_1)){ */
-/*     for (pi_2 = ID_permutation(p->row); pi_2!=last_permutation(ID_permutation(p->row)); pi_2 = next_permutation(pi_2)){ */
-/*       for (pi_3 = ID_permutation(p->row); pi_3!=last_permutation(ID_permutation(p->row)); pi_3 = next_permutation(pi_3)){ */
-/* 	if (pi_1 == pi_2 && pi_2 == pi_3){ */
-/* 	  countinue; */
-/* 	} */
-/* 	else{ */
-/* 	  result = -1; */
-/* 	} */
-/* 	for (u = 0;u< p->row;u++){ */
-/* 	  for (i = 0;i< p->column;i++ ){ */
-/* 	    if( (p->puzzle[Apply_permutation(pi_1, u)][i] = 1) && */
-/* 		(p->puzzle[Apply_permutation(pi_2, u)][i] = 2) && */
-/* 		(p->puzzle[Applu_permutation(pi_3, u)][i] != 3) ){ */
-/* 	      result = 1; */
-/* 	    }else if((p->puzzle[Apply_permutation(pi_1, u)][i] != 1) && */
-/* 		(p->puzzle[Apply_permutation(pi_2, u)][i] = 2) && */
-/* 		(p->puzzle[Applu_permutation(pi_3, u)][i] = 3) ){ */
-/* 	      result = 1; */
-/* 	    }else if (p->puzzle[Apply_permutation(pi_1, u)][i] = 1) && */
-/* 		(p->puzzle[Apply_permutation(pi_2, u)][i] != 2) && */
-/* 		(p->puzzle[Applu_permutation(pi_3, u)][i] = 3) ){ */
-/* 	      result = 1; */
-/* 	    }    */
-/* 	  } */
-/* 	} */
-/*       if (result == -1){ */
-/* 	return -1; */
-/*       } */
+  for (pi_1 = ID_permutation(p->row); !equals(pi_1,last_permutation(p->row)) ; pi_1 = next_permutation(pi_1)){
+    for (pi_2 = ID_permutation(p->row); !equals(pi_2,last_permutation(p->row)); pi_2 = next_permutation(pi_2)){
+      for (pi_3 = ID_permutation(p->row); !equals(pi_3,last_permutation(p->row)); pi_3 = next_permutation(pi_3)){
+	if (pi_1 == pi_2 && pi_2 == pi_3){
+	  continue;
+	}
+	else{
+	  result = -1;
+	}
+	for (u = 0;u< p->row;u++){
+	  for (i = 0;i< p->column;i++ ){
+	    if( (p->puzzle[Apply_permutation(pi_1, u)][i] == 1) &&
+		(p->puzzle[Apply_permutation(pi_2, u)][i] == 2) &&
+		(p->puzzle[Apply_permutation(pi_3, u)][i] != 3) ){
+	      result = 1;
+	    }else if((p->puzzle[Apply_permutation(pi_1, u)][i] != 1) &&
+		     (p->puzzle[Apply_permutation(pi_2, u)][i] == 2) &&
+		     (p->puzzle[Apply_permutation(pi_3, u)][i] == 3) ){
+	      result = 1;
+	    }else if ((p->puzzle[Apply_permutation(pi_1, u)][i] == 1) &&
+		      (p->puzzle[Apply_permutation(pi_2, u)][i] != 2) &&
+		      (p->puzzle[Apply_permutation(pi_3, u)][i] == 3) ){
+	      result = 1;
+	    }
+	  }
+	}
+      if (result == -1){
+	return -1;
+      }
 
-/*       } */
-/*     } */
-/*   } */
-/*   return 1; */
-/* } */
+      }
+    }
+  }
+  return 1;
+}
 
 int main(int argc, char * argv[]){
   
