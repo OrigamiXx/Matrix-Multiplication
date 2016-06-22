4b25d42e96636bc6ec07ff683aef576319d70dea
Jerry <jiz@union.edu>
2016-06-22 14:54:55 -0400

    add the 3-dimension array and shrink the runtime of CheckUSP

--- usp.c
+++ usp.c
@@ -31,27 +31,21 @@ int check_all_usp(int row, int column){
       for(k=0; k<max_index; k++){
 	
 	puzzle * p = create_puzzle_from_index(i, j, k);
-	//printf("%d\n",check_usp(p));
 	int res_check = check_usp(p);
 	int res_mult = res_check; //  check_usp_mult(p);
 
 	if(res_check && res_mult){
-          //print_puzzle(p);
-	  //printf("----------\n");
 	 tt++;
 	  write_puzzle(p, k);  
 	}
 	if(!res_check && !res_mult){
 	  ff++;
-	  //print_puzzle(p);
 	}
 	if(!res_check && res_mult){
 	  ft++;
-	  //print_puzzle(p);
 	}
 	if(res_check && !res_mult){
 	  tf++;
-	  //print_puzzle(p);
 	}
 	
 	destroy_puzzle(p);
@@ -66,95 +60,90 @@ int check_all_usp(int row, int column){
   return 0;
 }
 
+//check one case of pi1 pi2 pi3 holds the strong usp property
+int check_usp_rows(int row1, int row2, int row3, puzzle * p){
+  int i;
+  for (i = 0;i< p->column;i++ ){
+       
+     if( (p->puzzle[row1][i] == 1) &&
+	 (p->puzzle[row2][i] == 2) &&
+	 (p->puzzle[row3][i] != 3) ){
+       return true;
+     }else if((p->puzzle[row1][i] != 1) &&
+	      (p->puzzle[row2][i] == 2) &&
+	      (p->puzzle[row3][i] == 3) ){
+       return true;
+     }else if ((p->puzzle[row1][i] == 1) &&
+	       (p->puzzle[row2][i] != 2) &&
+	       (p->puzzle[row3][i] == 3) ){
+       return true;
+     }
+   }
+  return false;
 
-
-
+}
 int check_usp(puzzle * p){
   perm * pi_1, * pi_2, * pi_3;
-  int u, i, result;
+  int u, result;
   result = false;
-
-  //printf("Starting check_usp\n");
-
-  /* print(create_perm_identity(p->row)); */
-
-  /* printf("\n"); */
-
-  /* print(last_perm(p->row)); */
-
-  /* printf("\n"); */
-
-  /* pi_1 = create_perm_identity(p->row); */
-
-  /* printf("!equal = %d\n",!is_equals_perm(pi_1,last_perm(p->row))); */
-
-  int count = 0;
-  for (pi_1 = create_perm_identity(p->row); ; pi_1 = next_perm(pi_1)){
-    //printf("count = %d\n",count);
-    count++;
-    //printf("%d\n", is_last_perm(pi_1));
-    //printf("pi_1 = \n");
-    //print_compact_perm(pi_1);
-    //printf("\n");
-    for (pi_2 = create_perm_identity(p->row); ; pi_2 = next_perm(pi_2)){
-      //printf("pi_2 = \n");
-      //print_compact_perm(pi_2);
-      //printf("\n");
-      for (pi_3 = create_perm_identity(p->row); ; pi_3 = next_perm(pi_3)){
-	//	printf("pi_3 = \n");
-	//print_compact_perm(pi_3);
-	//printf("\n");
-	if (!(is_equals_perm(pi_1,pi_2) && is_equals_perm(pi_2, pi_3))){
-
-	  result = false;
-
-	  for (u = 0;u< p->row;u++){
-	    //printf("u = %d\n",u);
-	    for (i = 0;i< p->column;i++ ){
-	      //printf("pi1(u) = %d\n", apply_perm(pi_1, u));
-	      //printf("pi2(u) = %d\n", apply_perm(pi_2, u));
-	      //printf("pi3(u) = %d\n", apply_perm(pi_3, u));
-	      if( (p->puzzle[apply_perm(pi_1, u)][i] == 1) &&
-		  (p->puzzle[apply_perm(pi_2, u)][i] == 2) &&
-		  (p->puzzle[apply_perm(pi_3, u)][i] != 3) ){
-		result = true;
-	      }else if((p->puzzle[apply_perm(pi_1, u)][i] != 1) &&
-		       (p->puzzle[apply_perm(pi_2, u)][i] == 2) &&
-		       (p->puzzle[apply_perm(pi_3, u)][i] == 3) ){
-		result = true;
-	      }else if ((p->puzzle[apply_perm(pi_1, u)][i] == 1) &&
-			(p->puzzle[apply_perm(pi_2, u)][i] != 2) &&
-			(p->puzzle[apply_perm(pi_3, u)][i] == 3) ){
-		result = true;
-	      }
-	      //printf("result = %d\n",result);
-	    }
-	  }
-	  
-	  if (!result){
-	    return false;
-	  }
-
-	}
-
-	//printf("break?\n");
-
-	if(is_last_perm(pi_3)){
-	  break;
-	}
-      }
-      destroy_perm(pi_3);
-      if(is_last_perm(pi_2)){
-	break;
+  int row_result[20][20][20];
+  int i, j, k;
+  for(i = 0; i < p->row; i++){
+    for(j = 0; j < p->row; j++){
+      for(k = 0; k < p->row; k++){
+	row_result[i][j][k] = check_usp_rows(i, j, k, p);	
       }
     }
-    destroy_perm(pi_2);
-    if(is_last_perm(pi_1)){
+    
+  }
+  //printf("Starting check_usp\n");
+  //for (pi_1 = create_perm_identity(p->row); ; pi_1 = next_perm(pi_1)){
+  pi_1 = create_perm_identity(p->row);
+  //printf("pi_1 = \n");
+  //print_compact_perm(pi_1);
+  //printf("\n");
+  for (pi_2 = create_perm_identity(p->row); ; pi_2 = next_perm(pi_2)){
+    //printf("pi_2 = \n");
+    //print_compact_perm(pi_2);
+    //printf("\n");
+    for (pi_3 = create_perm_identity(p->row); ; pi_3 = next_perm(pi_3)){
+      //printf("pi_3 = \n");
+      //print_compact_perm(pi_3);
+      //printf("\n");
+      if (!(is_equals_perm(pi_1,pi_2) && is_equals_perm(pi_2, pi_3))){
+	
+	result = false;
+	
+	for (u = 0;u<p->row && !result;u++){
+	  int a, b, c;
+	  a= apply_perm(pi_1, u);
+	  b= apply_perm(pi_2, u);
+	  c= apply_perm(pi_3, u);
+	  result = row_result[a][b][c];
+	}
+	
+	if (!result){
+	  return false;
+	}
+	
+      }
+      
+      if(is_last_perm(pi_3)){
+	break;
+	}
+    }
+    destroy_perm(pi_3);
+    if(is_last_perm(pi_2)){
       break;
     }
   }
-
+  destroy_perm(pi_2);
+  //if(is_last_perm(pi_1)){
+  //break;
+    // }
+    //}
+  
   destroy_perm(pi_1);
-
+  
   return true;
 }
