c28d3728dcfcb7e13d50029a551ff32f38e42634
Anthony Xu <xua@crochet-02.union.edu>
2017-06-15 17:23:58 -0400

    attempt on 6*14

--- usp_tester.c
+++ usp_tester.c
@@ -24,13 +24,35 @@ int main(int argc, char * argv[]){
   //printf("%d\n",sizeof(long));
   //printf("%d\n",sizeof(long long));
   
-  int row = 3;
-  int column = 5;
-  //int index = 1;
-  //puzzle * p = create_puzzle_from_index(row,column,index);
-  //print_puzzle(p);
-  //check_all_usp(row, column);
-  check_usp_same_col(row,column);
+   int * puzzle1 = (int *) malloc(sizeof(int *)*14);
+  puzzle1[0] = 37;
+  puzzle1[1]= 604;
+  puzzle1[2]= 550;
+  puzzle1[3]= 19;
+  puzzle1[4] = 154;
+  puzzle1[5] = 706;
+  puzzle1[6] = 535;
+  puzzle1[7] = 245;
+  puzzle1[8] = 173;
+  puzzle1[9] = 302;
+  puzzle1[10] = 221;
+  puzzle1[11] = 50;
+  puzzle1[12] = 455;
+  puzzle1[13] = 158;
+
+  int givenR = 14;
+  int givenC = 6;
+
+  puzzle * result = (puzzle *) (malloc(sizeof(puzzle)));
+  result->row = givenR;
+  result->column = givenC;
+  result->pi = create_perm_identity(result->row);
+  result -> puzzle = puzzle1;
+
+  print_puzzle(result);
+
+  //check_usp(result);
+  printf("%d", check_usp(result));
   
   return 0;
 
