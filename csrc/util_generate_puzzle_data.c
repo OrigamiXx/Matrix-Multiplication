5edb42f690a264bef971e00dd8cb955b4f72fa99
Matthew Anderson <andersm2@union.edu>
2019-04-19 16:11:52 -0400

    Fixed off by one and str formating.

--- csrc/util_generate_puzzle_data.c
+++ csrc/util_generate_puzzle_data.c
@@ -17,7 +17,7 @@ int main(int argc, char * argv[]){
 
   fprintf(data_file, "%s", "Row1,Row2,Row3,isSUSP,#of1s,#of2s,#of3s\n");
 
-  for(; index < pow(3, givenR * givenC) -1; index+=1){
+  for(; index < pow(3, givenR * givenC); index+=1){
     puzzle * p;
     p = create_puzzle_from_index(givenR, givenC, index);
 
@@ -36,26 +36,25 @@ int main(int argc, char * argv[]){
         strncpy(isUSB, "False", sizeof(isUSB));
     }
     for(i = 0; i < p -> s; i++){
-	      for(j = 0; j < p -> k; j++){
-	          fprintf(data_file, "%d", get_entry(p, i, j));
-
-            switch (get_entry(p, i, j))
-            {
-              case 1: count1s++;
-                  break;
-              case 2: count2s++;
-                  break;
-              case 3: count3s++;
-                  break;
-            }
+      for(j = 0; j < p -> k; j++){
+	fprintf(data_file, "%d", get_entry(p, i, j));
+	
+	switch (get_entry(p, i, j))
+	  {
+	  case 1: count1s++;
+	    break;
+	  case 2: count2s++;
+	    break;
+	  case 3: count3s++;
+	    break;
+	  }
       }
       fprintf(data_file, ",");
     }
-    fprintf(data_file, "%s%s", isUSB, ",");
-    fprintf(data_file, "%d%s", count1s, ",");
-    fprintf(data_file, "%d%s", count2s, ",");
+    fprintf(data_file, "%s,", isUSB);
+    fprintf(data_file, "%d,", count1s);
+    fprintf(data_file, "%d,", count2s);
     fprintf(data_file, "%d", count3s);
-
     fprintf(data_file, "\n");
     destroy_puzzle(p);
   }
